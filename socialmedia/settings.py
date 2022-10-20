894f84ee6ddd072e39c4ff865d9b7aeecb4474bc
Gooey Code <gooeycode@gmail.com>
2022-10-20 23:27:01 +0100

    password change

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -137,3 +137,11 @@ LOGIN_REDIRECT_URL = "home"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = "gooeycode@gmail.com"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = "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"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
