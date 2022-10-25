0bcd0b53c2d40df3975cb29cdcfeaf05d40376b2
Gooey Code <gooeycode@gmail.com>
2022-10-25 19:10:36 +0100

    mixin authorisations

--- socialpost/views.py
+++ socialpost/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import SocialPost
@@ -12,15 +12,23 @@ class SocialPostDetailView(LoginRequiredMixin, DetailView):
     model = SocialPost
     template_name = 'socialpost_detail.html'
 
-class SocialPostUpdateView(LoginRequiredMixin, UpdateView):
+class SocialPostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = SocialPost
     fields = ('title', 'body',)
     template_name = 'socialpost_edit.html'
 
-class SocialPostDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class SocialPostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = SocialPost
     template_name = 'socialpost_delete.html'
     success_url = reverse_lazy('socialpost_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 class SocialPostCreateView(LoginRequiredMixin, CreateView):
     model = SocialPost
