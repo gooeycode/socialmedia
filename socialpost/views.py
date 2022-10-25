1e8474a78780f27a066631e75faea52691aab9c8
Gooey Code <gooeycode@gmail.com>
2022-10-25 08:53:43 +0100

    new venv and user authentication, and page permissions

--- socialpost/views.py
+++ socialpost/views.py
@@ -1,29 +1,34 @@
-from nntplib import ArticleInfo
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import SocialPost
 from django.urls import reverse_lazy
 
-class SocialPostListView(ListView):
+class SocialPostListView(LoginRequiredMixin, ListView):
     model = SocialPost
     template_name = 'socialpost_list.html'
 
-class SocialPostDetailView(DetailView):
+class SocialPostDetailView(LoginRequiredMixin, DetailView):
     model = SocialPost
     template_name = 'socialpost_detail.html'
 
-class SocialPostUpdateView(UpdateView):
+class SocialPostUpdateView(LoginRequiredMixin, UpdateView):
     model = SocialPost
     fields = ('title', 'body',)
     template_name = 'socialpost_edit.html'
 
-class SocialPostDeleteView(DeleteView):
+class SocialPostDeleteView(LoginRequiredMixin, DeleteView):
     model = SocialPost
     template_name = 'socialpost_delete.html'
     success_url = reverse_lazy('socialpost_list')
 
-class SocialPostCreateView(CreateView):
+class SocialPostCreateView(LoginRequiredMixin, CreateView):
     model = SocialPost
     template_name = 'socialpost_create.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body',)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 
